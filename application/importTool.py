d488d99db5fe0bb2ba63671bf7cb2861abdf8050
Jukka Uusisalo <jukka.uusisalo@gmail.com>
2023-08-25 20:02:36 +0300

    Issue-6. Need test in windows box also. (#12)

--- application/importTool.py
+++ application/importTool.py
@@ -6,17 +6,28 @@ import importmodule
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,	help="path to input csv")
-ap.add_argument("-c", "--command", required=True,	help="Import tool commands")
+ap.add_argument("-c", "--command", required=False,default="import",	help="Import tool commands")
+ap.add_argument("-k","--keep",required=False, help="keep existing data in database", action='store_true')
 args = vars(ap.parse_args())
 
 command = args.get('command')
-csvFile = args.get('input');
+csvFile = args.get('input')
+keep = args.get('keep')
 
-print (command + ' / ' + csvFile)
+if(command =='import'): 
 
-competitors = importmodule.readCvs(csvFile)
-for licenseNo in competitors:
-    competitor = competitors[licenseNo]
-    print(competitor.licenseNo + ' ' + competitor.name + ' ' + competitor.email )
+    if(args.get('keep') is False):
+        print ('Clear db')
+        importmodule.clearDB()
+    else:
+        print ('Keep existing data in db')
+
+    print (command + ' / ' + csvFile)
+    competitors = importmodule.readCvs(csvFile)
+    for licenseNo in competitors:
+        competitor = competitors[licenseNo]
+        print(competitor.licenseNo + ' ' + competitor.name + ' ' + competitor.email )
+else:
+    print ('command ' + command + ' is not supported')
 
 
